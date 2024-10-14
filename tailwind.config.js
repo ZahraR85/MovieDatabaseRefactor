445fa92d0ce98358f924290a6e95b0ee090a9585
Shabnam <zahra.reihani85@gmail.com>
2024-10-14 17:35:40 +0200

    parcel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-/**
- * color scheme extracted from hero image using https://color.adobe.com/de/create/image
- * #A60321
- * #6E8C03
- * #D98E04
- * #BF4904
- * #26140D
- * and exported via https://uicolors.app/create 
- * 
- * use those like 'bg-mexican-red-300' or 'text-mexican-red-300
- */
-
-tailwind.config = {
-  darkMode: 'class',
+module.exports = {
+  content: [
+    './modules/**/*.{html,js}',
+'./index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
